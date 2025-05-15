1997083d953ec0da445046693a98094e4e4db527
bvtterrflie <jaeminburgerrr@gmail.com>
2025-05-15 23:19:42 +0700

    tugas & praktikum 6

--- Tugas/TUGAS 6/index.php
+++ Tugas/TUGAS 6/index.php
@@ -12,27 +12,41 @@
         h2 {
             margin: 0;
         }
+        input[type="number"] {
+            width: 60px;
+            padding: 5px;
+        }
+        button {
+            padding: 5px 10px;
+            margin-top: 10px;
+        }
     </style>
 </head>
 <body>
 
 <?php
-// Data barang menggunakan array
+// Data barang
 $barang = [
     "nama" => "Keyboard",
     "harga" => 150000
 ];
 
-// Jumlah beli disimpan dalam variabel
-$jumlah_beli = 2;
-
-// Pajak sebagai konstanta (10%)
+// Pajak
 define("PAJAK", 0.10);
 
-// Perhitungan
-$total_sebelum_pajak = $barang["harga"] * $jumlah_beli;
-$jumlah_pajak = $total_sebelum_pajak * PAJAK;
-$total_bayar = $total_sebelum_pajak + $jumlah_pajak;
+// Default nilai
+$jumlah_beli = 0;
+$total_sebelum_pajak = 0;
+$jumlah_pajak = 0;
+$total_bayar = 0;
+
+// Jika form disubmit
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $jumlah_beli = (int)$_POST["jumlah_beli"];
+    $total_sebelum_pajak = $barang["harga"] * $jumlah_beli;
+    $jumlah_pajak = $total_sebelum_pajak * PAJAK;
+    $total_bayar = $total_sebelum_pajak + $jumlah_pajak;
+}
 ?>
 
 <div class="box">
@@ -40,10 +54,19 @@ $total_bayar = $total_sebelum_pajak + $jumlah_pajak;
     <hr>
     <p>Nama Barang: <?= $barang["nama"] ?></p>
     <p>Harga Satuan: Rp <?= number_format($barang["harga"], 0, ',', '.') ?></p>
-    <p>Jumlah Beli: <?= $jumlah_beli ?></p>
-    <p>Total Harga (Sebelum Pajak): Rp <?= number_format($total_sebelum_pajak, 0, ',', '.') ?></p>
-    <p>Pajak (10%): Rp <?= number_format($jumlah_pajak, 0, ',', '.') ?></p>
-    <p><b>Total Bayar: Rp <?= number_format($total_bayar, 0, ',', '.') ?></b></p>
+
+    <form method="post">
+        <label for="jumlah_beli">Jumlah Beli:</label>
+        <input type="number" name="jumlah_beli" id="jumlah_beli" min="1" value="<?= $jumlah_beli ?>" required>
+        <br>
+        <button type="submit">Hitung</button>
+    </form>
+
+    <?php if ($jumlah_beli > 0): ?>
+        <p>Total Harga (Sebelum Pajak): Rp <?= number_format($total_sebelum_pajak, 0, ',', '.') ?></p>
+        <p>Pajak (10%): Rp <?= number_format($jumlah_pajak, 0, ',', '.') ?></p>
+        <p><b>Total Bayar: Rp <?= number_format($total_bayar, 0, ',', '.') ?></b></p>
+    <?php endif; ?>
 </div>
 
 </body>
